853c8cb3472e4290fc93e7b69b90d52c1c405d8f
maximus1200 <kevin.herbert@gmx.at>
2020-04-18 14:38:32 +0200

    transponder check stage 1

--- inc/transponder_check.php
+++ inc/transponder_check.php
@@ -6,23 +6,29 @@
     $employee_number = $_POST['employee_number'];
 
     /* SQL Statement + Query */
-    $sql = "SELECT number, lastname ,authorized FROM employee WHERE number = '$employee_number' AND authorized = 1;";
+    /*$sql = "SELECT number, lastname ,authorized FROM employee WHERE number = '$employee_number' AND authorized = '1';";*/
+    $sql = "SELECT number, lastname ,authorized FROM employee WHERE number = '$employee_number';";
     $result = mysqli_query($con, $sql);
 
     while ($row =mysqli_fetch_assoc($result)) {
         $numberFromDB = $row['number'];
+        $authorizedFromDB = $row['authorized'];
     }
 
     /*wenn nichts eingegeben wir kommt trotzdem grün !!!!!!!!! */
     /* Copare the data from the chip with data from DB and start session (green and red) */
-    if ($numberFromDB == $employee_number) {
+    if ($numberFromDB == $employee_number and $authorizedFromDB == '1') {
         session_start();
-        $_SESSION['check_number'] = $employee_number;
+        $_SESSION['check_number_green'] = $employee_number;
         header('Location: green.php');
     }
-    else{
+    elseif ($numberFromDB == $employee_number and $authorizedFromDB == '0') {
+        session_start();
+        $_SESSION['check_number_yellow'] = $employee_number;
         header('Location: yellow.php');
     }
 
-    
+    else {
+        header('Location: red.php');
+    }
 ?>
